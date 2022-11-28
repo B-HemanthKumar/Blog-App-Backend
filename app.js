7138f6e52801cfadfef88bb9735a8a63cb3d603c
B-HemanthKumar <hemanthpandu328@gmail.com>
2022-11-28 15:37:11 +0530

    added origin in cors

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ import blogRouter from "./routes/blog-routes";
 import router from "./routes/user-routes";
 import cors from "cors";
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:3000",
+}));
 
-app.use (function (req, res, next) { 
-  res.header ("Access-Control-Allow-Origin", "*"); 
-  res.header ("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next (); });
+// app.use (function (req, res, next) { 
+//   res.header ("Access-Control-Allow-Origin", "http://localhost:3000"); 
+//   res.header ("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next (); 
+// });
 
 app.use(express.json());
 app.use("/api/user", router);
